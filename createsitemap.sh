697dac6f5d75f92a538364b486f0b8cc907b8265
Peter Law <PeterJCLaw@gmail.com>
2013-09-19 20:59:10 +0100

    Replace the sitemap with a generator script.
    Update the existing creation script to invoke linkchecker and then
    a new python script which generates the actual sitemap. This script
    collects together the set of links that we want (including filtering
    out various things that don't need to be listed) & outputs sitemap xml.
    
    This will be run via puppet, whenever the live site is updated.
    
    Change-Id: Ief2c724207634eb93bfbd4539d16723085b84a82

--- createsitemap.sh
+++ createsitemap.sh
@@ -1,31 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-ROOT_URI="https://www.studentrobotics.org/"
+if [ -z "$1" ]
+then
+	echo "Usage: $0 ROOT_URI"
+	echo "  ROOT_URI: the web root to crawl."
+	exit 1
+fi
 
-echo '<?xml version="1.0" encoding="UTF-8"?>' > sitemap.xml
-echo "<urlset\
- xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\
- xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\
- xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9\
-  http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">" >> sitemap.xml
+ROOT_URI=$1
+# Ensure single trailing slash
+ROOT_URI=${ROOT_URI%*/}/
 
-URLS=$( find content/default/ |
-	grep --invert-match -E '\..*$' |
-	grep --invert-match -E '\/index$' |
-	awk '{ sub(/^content\/default\//, ""); print }' |
-	xargs -I {} echo {}
-)
-
-for i in $URLS; do
-
-	if [ -d "content/default/$i" ]; then
-		echo "$i/"
-	else
-		echo $i
-	fi
-
-done | xargs -I {} echo -e "\n\t<url>\n\t\t<loc>$ROOT_URI{}</loc>\n\t</url>" >> sitemap.xml
-
-echo '</urlset>' >> sitemap.xml
+# Add -F xml for debugging to also outupt to a file
+linkchecker "$ROOT_URI" -o xml -v \
+	--ignore-url="$ROOT_URI(forum|docs/python/|ide|tickets|userman|search.php)" \
+	--ignore-url="!$ROOT_URI" \
+	| python sitemapgen.py - "$ROOT_URI" > sitemap.xml
 
 exit 0
