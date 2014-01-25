d0693b2a7cb0698a045bd88ee9f0fdb1845d2fcc
Peter Law <PeterJCLaw@gmail.com>
2014-01-25 13:04:23 +0000

    Fix #2156: Add dummy team-status data.
    This makes it relatively simple to examine the teams portion of srweb
    without needing to have an IDE setup to produce the data. The provided
    data uses the SRZ team, which already existed partly for this purpose.
    
    NB: This relies on change Iaa8f04c00694bbc8 in puppet to avoid disrupting
    the live site.
    
    Change-Id: If99b130ed3bad93046cc7bad2b0cd0b013d07d85

--- config.inc.php
+++ config.inc.php
@@ -171,10 +171,10 @@ default_define('TEMPLATE_DIR', 		ROOT_DIR . '/templates');
 default_define('COMPILED_TEMPLATE_DIR', ROOT_DIR . '/templates_compiled');
 default_define('CACHE_DIR',		ROOT_DIR . '/cache');
 default_define('CONTENT_DIR', 		ROOT_DIR . '/content');
-default_define('TEAM_STATUS_DIR', 	ROOT_DIR . '/ide/settings/team-status');
+default_define('TEAM_STATUS_DIR', 	ROOT_DIR . '/dev-content/team-status');
 // The following is used in both a template and a parsed page so ROOT_DIR
 // is prepended automatically in the parsed page and manually in the template
-default_define('TEAM_STATUS_IMG',	'images/teams');
+default_define('TEAM_STATUS_IMG',	'images/dev-teams');
 
 // changes each SR year
 default_define('TEAMS_JSON', 'resources/2014/teams.json');
