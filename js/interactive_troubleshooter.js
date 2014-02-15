873084b358eeb66c8efad9bff12ca3a41e691fe3
Tom Leese <leese.thomas81@gmail.com>
2014-02-15 16:05:29 +0000

    Add the interactive troubleshooter
    This patch implements the interactive troubleshooter as per trac
    ticket #1594 and #1011. A link has been added to the documentation page
    to access the troubleshooter.
    
    The troubleshooter consists of 4 main files: data.json contains the tree
    of questions and answers, main.js is the main logic for the
    troubleshooter, styles.css contains styles specific to the
    troubleshooter and then interactive_troubleshooter connects everything
    together.
    
    This commit is a squashed commit from the troubleshooter branch of
    srweb-devel to ease merging via gerrit and the work has been done by
    Chris Baines, Tom Leese, Freddie Nash and Helena Weare.
    
    Change-Id: I02b382c37041bf4ad5afccc63434ada02ddff22a

---
+++ js/interactive_troubleshooter.js
@@ -0,0 +1,119 @@
+(function() {
+    var data = { };
+    var globalRadioName = 0;
+
+    function init() {
+        var resetBtnAtTop = document.getElementById("resetbtnattop");
+        resetBtnAtTop.onclick = function() {
+            window.location.hash = "";
+            reset();
+        };
+
+        reset();
+    }
+
+    function reset() {
+        globalRadioName = 0;
+
+        var resetBtnAtTop = document.getElementById("resetbtnattop");
+        resetBtnAtTop.style.display = "none";
+
+        var troubleshooter = document.getElementById("troubleshooter");
+        troubleshooter.innerHTML = "";
+
+        if (window.location.hash != "") {
+            resetBtnAtTop.style.display = "inline-block";
+        }
+
+        var id = window.location.hash.substr(1); // substr to remove # on fragment
+        appendQuestionById(troubleshooter, id);
+    }
+
+    function answerClicked(radioBtn, parent, answer) {
+        parent.innerHTML = "";
+
+        if ("message" in answer) {
+            var newMessage = document.createElement('p');
+            newMessage.className = "message";
+            newMessage.innerHTML = answer.message;
+            parent.appendChild(newMessage);
+        }
+
+        if ("askiffixed" in answer) {
+            var question = data.askiffixed_question;
+            question.answers[1].nextquestion = answer.nextquestion;
+            appendQuestion(parent, question);
+            return;
+        }
+
+        if (!("nextquestion" in answer)) {
+            var newMessage = document.createElement("p");
+            newMessage.className = "message";
+            newMessage.textContent = "Thank you for using the SR Troubleshooter.";
+            parent.appendChild(newMessage);
+
+            var resetBtnAtTop = document.getElementById("resetbtnattop");
+            resetBtnAtTop.style.display = "inline-block";
+
+            return;
+        }
+
+        appendQuestionById(parent, answer.nextquestion);
+    }
+
+    function appendQuestionById(parent, id) {
+        window.location.hash = "#" + id;
+        var question = data.questions[id];
+        appendQuestion(parent, question);
+    }
+
+    function appendQuestion(parent, question) {
+        var newQuestionCont = document.createElement("div");
+
+        var newQuestionDiv = document.createElement("div");
+        newQuestionDiv.className = "question";
+
+        var questionParagraph = document.createElement("p");
+        questionParagraph.innerHTML = question.question;
+        newQuestionDiv.appendChild(questionParagraph);
+
+        var questionForm = document.createElement("form");
+        newQuestionDiv.appendChild(questionForm);
+
+        globalRadioName++;
+
+        for (var i = 0; i < question.answers.length; i++) {
+            (function(answer) {
+                var answerLabel = document.createElement("label");
+
+                var answerRadioBtn = document.createElement("input");
+                answerRadioBtn.type = "radio";
+                answerRadioBtn.name = globalRadioName;
+                answerRadioBtn.onclick = function() {
+                    answerClicked(answerRadioBtn, newQuestionCont, answer);
+                };
+
+                var answerSpan = document.createElement("span");
+                answerSpan.textContent = answer.answer;
+
+                answerLabel.appendChild(answerRadioBtn);
+                answerLabel.appendChild(answerSpan);
+                questionForm.appendChild(answerLabel);
+            }(question.answers[i]))
+        }
+
+        parent.appendChild(newQuestionDiv);
+        parent.appendChild(newQuestionCont);
+    }
+
+    jQuery.getJSON(TroubleshooterDataURL, function(json, textStatus, xhr) {
+        data = json;
+
+        if (data) {
+            init();
+        } else {
+            var noQuestions = document.getElementById("troubleshooter_no_questions");
+            noQuestions.style.display = "block";
+        }
+    });
+}());
