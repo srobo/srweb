cfdc092ccdab4efd81f5d578525b01aa9153595e
Peter Law <PeterJCLaw@gmail.com>
2014-03-15 11:09:20 +0000

    Add a helper for generating the league table's rows.
    This doens't touch the DOM, instead performing just the data
    conversion required to show the data we want. At the moment it doesn't
    handle the case of a tie which is on the cutoff row (which did occur
    in SR2013), I'll fix that next.
    
    Change-Id: Ia5ba053759c0109fe7da369fac84338390e964b5

---
+++ js/competition-utils.js
@@ -0,0 +1,39 @@
+
+var league_sorter = function() {
+    var sort_helper = function(a, b) {
+        return b.points - a.points;
+    }
+    var display_convert = function(rows, cutoff) {
+        var last_score = 0;
+        var last_pos = 1;
+        for (var i=0; i<rows.length; i++) {
+            var row = rows[i];
+            if (row.points == last_score) {
+                row.points = '';
+                row.pos = '';
+            } else {
+                last_score = row.points;
+                last_pos = row.pos = i+1;
+            }
+        }
+        if (cutoff != null && rows.length > cutoff) {
+            rows.splice(cutoff, 0, {'tla': '-', 'points': '-', 'pos': '-'});
+        }
+    }
+    return function(points, cutoff) {
+        var rows = [];
+
+        for (var tla in points) {
+            var pts = points[tla];
+            rows.push({'tla': tla, 'points': pts});
+        }
+        rows.sort(sort_helper);
+        display_convert(rows, cutoff);
+        return rows;
+    };
+}();
+
+// node require() based exports.
+if (typeof(exports) != 'undefined') {
+    exports.league_sorter = league_sorter;
+}
