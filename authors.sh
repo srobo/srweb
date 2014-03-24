a41b202f970168a50dbc82d57c462da514855c98
Alastair Lynn <arplynn@gmail.com>
2014-03-24 21:20:52 +0000

    Update AUTHORS with the current contributors

--- authors.sh
+++ authors.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git log --format='%aN <%aE>' | 
 	awk '{arr[$0]++} END{for (i in arr){print arr[i], i;}}' | 
+	grep -v BRIAN | 
 	sort -rn | 
 	grep -v "fail@studentrobotics.org" | 
 	cut -d\  -f2- > AUTHORS
