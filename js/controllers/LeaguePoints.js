28802465a44c5f5ed1219f01c6a0eda35574b79a
Peter Law <PeterJCLaw@gmail.com>
2014-04-17 20:43:33 +0100

    Remove the knockout entry limit.
    I'd completely forgotten that _all_ the teams are in the knockouts,
    not just a subset of them. This makes displaying the limit pointless.
    
    Change-Id: I0ebd97fb4f4b9d27a78346c548c9357d29600788

--- js/controllers/LeaguePoints.js
+++ js/controllers/LeaguePoints.js
@@ -4,12 +4,11 @@ var app = angular.module('app', ["ngResource", "competitionFilters"]);
 app.controller("LeaguePoints", function($scope, $resource) {
     var Points = $resource(API_ROOT + "/scores/league");
     var Teams = $resource(SRWEB_ROOT + "teams-data.php");
-    var KnockoutInfo = $resource(API_ROOT + "/knockout/max_entrants");
 
     $scope.knockout_max = null;
     var updateState = function() {
         Points.get(function(points) {
-            $scope.league_points = league_sorter(points.league_points, $scope.knockout_max, points.game_points);
+            $scope.league_points = league_sorter(points.league_points, null, points.game_points);
             $scope.game_points = gamepoints_sorter(points.game_points);
             $scope.latest_match = points.last_scored;
         });
@@ -18,10 +17,7 @@ app.controller("LeaguePoints", function($scope, $resource) {
         });
     };
 
-    KnockoutInfo.get(function(knockout) {
-        $scope.knockout_max = knockout.max_entrants;
-        // refresh every 10s
-        setInterval(updateState, 10000);
-        updateState();
-    });
+    // refresh every 10s
+    setInterval(updateState, 10000);
+    updateState();
 });
