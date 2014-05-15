b42237d51086947a176e04cfffce9df7df3e06c8
Peter Law <PeterJCLaw@gmail.com>
2014-05-15 20:25:35 +0100

    Ensure BRIAN LOG FEEDS doesn't appear in the AUTHORS.
    This isn't a real person, and as it may have originated from multiple
    people, it's not appropriate to just redirect it with a mailmap entry.
    
    Change-Id: I8a7ce32377c2559fb0d8659f931b40ef6ebc01a8

--- authors.sh
+++ authors.sh
@@ -2,6 +2,7 @@
 git log --format='%aN <%aE>' | 
 	awk '{arr[$0]++} END{for (i in arr){print arr[i], i;}}' | 
 	sort -rn | 
+	grep -v "fail@studentrobotics.org" | 
 	cut -d\  -f2- > AUTHORS
 
 if [ "$1" == "-e" ]
