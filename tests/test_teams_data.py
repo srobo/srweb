d171d56cd93b1263d8caadc57cab19e58abe3135
Alistair Lynn <arplynn@gmail.com>
2015-02-20 20:50:36 +0000

    Add 'rookie' flag to rookie teams in teams.json
    This will be used in srcomp for computation of the winner of the rookie
    award.
    
    Patch Set #2: Fix syntax error, add tests
    
    Change-Id: I3cda63a6b8f806b2864b5fafdba7e9753a674de9

--- tests/test_teams_data.py
+++ tests/test_teams_data.py
@@ -6,9 +6,12 @@ import helpers
 def test_data_file():
     data = helpers.ensure_valid_json('resources/2015/teams.json')
 
-    for info in data.values():
+    data_previous = helpers.ensure_valid_json('resources/2014/teams.json')
+
+    for tla, info in data.items():
         assert 'URL' in info
         assert 'name' in info
+        assert 'rookie' in info
 
         url = info['URL']
         assert len(url) > 0
@@ -18,3 +21,8 @@ def test_data_file():
 
         name = info['name']
         assert len(name) > 0
+
+        rookie = info['rookie']
+        assert isinstance(rookie, bool)
+        if rookie:
+            assert tla not in data_previous
